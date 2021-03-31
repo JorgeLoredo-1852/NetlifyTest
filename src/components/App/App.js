797df778c549a00cdab595b634ad8f03b1bc0f48
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2021-03-30 20:18:18 -0600

    HotFix Sidebar responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,21 +3,23 @@ import "../../global-style/_base.scss";
 import "./_App.scss";
 
 import Header from "../Header/Header";
-//import SideBar from "../SideBar/SideBar";
-//import Hotel from "../Hotel/Hotel";
-import { GoogleLogin } from "react-google-login";
+import SideBar from "../SideBar/SideBar";
+import Hotel from "../Hotel/Hotel";
+//import { GoogleLogin } from "react-google-login";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
-      <div className="content">{/*<SideBar />
-        <Hotel />*/}</div>
-      <GoogleLogin
+      <div className="content">
+        <SideBar />
+        <Hotel />
+      </div>
+      {/*<GoogleLogin
         clientId="573398896027-s2gkdto402okk0v9ie2sp7u97oh7nekj.apps.googleusercontent.com"
         onSuccess={() => console.log(this)}
         onFailure={() => console.log("failure")}
-      />
+      />*/}
     </div>
   );
 };
